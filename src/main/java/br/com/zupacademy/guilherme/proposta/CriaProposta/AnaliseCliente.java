3c68f12967434e876129b321328a38f041110f89
guilherme.veronezzi <veronezzi14@gmail.com>
2021-07-02 14:31:02 -0300

    docker compose error

--- src/main/java/br/com/zupacademy/guilherme/proposta/CriaProposta/AnaliseCliente.java
+++ src/main/java/br/com/zupacademy/guilherme/proposta/CriaProposta/AnaliseCliente.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "analise-api", url = "http://localhost:9999/api")
+@FeignClient(name = "analise-api", url = "${zup.analise_financeira.url}/api")
 public interface AnaliseCliente {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/solicitacao")
--- src/main/java/br/com/zupacademy/guilherme/proposta/cartao/ApiCartao.java
+++ src/main/java/br/com/zupacademy/guilherme/proposta/cartao/ApiCartao.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(url = "http://localhost:8888/api", name = "recursos-cartao")
+@FeignClient(url = "${zup.cartoes.api.url}/api/cartoes", name = "recursos-cartao")
 public interface ApiCartao {
 
 	@GetMapping("/cartoes?idProposta={id}")
